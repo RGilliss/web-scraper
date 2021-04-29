030e6638831c2fc2e0b3bb7dadb9004be077f161
Rye Gilliss <gilliss.rg@gmail.com>
2021-04-29 13:33:42 -0600

    grabs price of superstore item but only sometimes...

--- scraper.js
+++ scraper.js
@@ -1,14 +1,15 @@
 const rp = require('request-promise');
 const puppeteer = require('puppeteer');
 const $ = require('cheerio');
-const url = `https://www.reddit.com`;
-
+const url = `https://www.realcanadiansuperstore.ca/classic-scissor-tongs/p/21296404_EA`;
+const selector = `span[class="price__value selling-price-list__item__price selling-price-list__item__price--now-price__value"]`
 
 puppeteer
   .launch()
   .then(browser => browser.newPage())
   .then(page => page.goto(url).then(() => page.content()))
-  .then(html => $('h2', html).each(function() {
-    console.log($(this).text())
-  }))
+  .then(html => {
+    console.log(html)
+    console.log($(selector, html).html())
+  })
   .catch(err => console.log(err));
