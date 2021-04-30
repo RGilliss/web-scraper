3e3e097d8f12baf1e449c76cec43b0904d1ff0fe
Rye Gilliss <gilliss.rg@gmail.com>
2021-04-30 10:03:18 -0600

    cleaned up

--- scraper.js
+++ scraper.js
@@ -1,7 +1,7 @@
 const rp = require('request-promise');
 const puppeteer = require('puppeteer');
 const $ = require('cheerio');
-const url = `https://www.realcanadiansuperstore.ca/classic-scissor-tongs/p/21296404_EA`;
+const url = `https://www.realcanadiansuperstore.ca/Lawn,%20Garden%20&%20Patio/Grills%20&%20Outdoor%20Cooking/Bbqs,%20Smokers%20&%20Accessories/Grilling%20Cookware/everyday-essentials-bbq-tongs/p/21297769_EA`;
 const selector = `span[class="price__value selling-price-list__item__price selling-price-list__item__price--now-price__value"]`
 
 puppeteer
@@ -9,7 +9,6 @@ puppeteer
   .then(browser => browser.newPage())
   .then(page => page.goto(url).then(() => page.content()))
   .then(html => {
-    console.log(html)
     console.log($(selector, html).html())
   })
   .catch(err => console.log(err));
