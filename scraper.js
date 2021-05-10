8210e63257574c741cb5397940d4f8262508293f
Rye Gilliss <gilliss.rg@gmail.com>
2021-05-10 14:09:05 -0600

    attempting with different urls

--- scraper.js
+++ scraper.js
@@ -1,7 +1,7 @@
 const rp = require('request-promise');
 const puppeteer = require('puppeteer');
 const $ = require('cheerio');
-const url = `https://www.realcanadiansuperstore.ca/Lawn,%20Garden%20&%20Patio/Grills%20&%20Outdoor%20Cooking/Bbqs,%20Smokers%20&%20Accessories/Grilling%20Cookware/everyday-essentials-bbq-tongs/p/21297769_EA`;
+const url = `https://www.realcanadiansuperstore.ca/thanos-mech/p/21205197_EA`;
 const selector = `span[class="price__value selling-price-list__item__price selling-price-list__item__price--now-price__value"]`
 
 puppeteer
